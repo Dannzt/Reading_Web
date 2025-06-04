da475dc073bf0c5417a12435ae97612bb5ed8e25
Dan <111394311+Dannzt@users.noreply.github.com>
2025-06-04 20:31:48 -0300

    registro html

---
+++ web/views/script.js
@@ -0,0 +1,9 @@
+function aplicarTemaEscuro() {
+    // Campo para trocar cor fundo
+    document.body.style.backgroundColor = "black";
+    // Cor geral letras, não especificada
+    document.body.style.color = "white";
+}
+
+// Chamar quando necessário
+aplicarTemaEscuro();
