46e33d2e00cb135f94aff64adda539fde46fb889
Dan <111394311+Dannzt@users.noreply.github.com>
2025-06-25 05:24:12 -0300

    .

--- web/views/script.js
+++
@@ -1,9 +0,0 @@
-function aplicarTemaEscuro() {
-    // Campo para trocar cor fundo
-    document.body.style.backgroundColor = "black";
-    // Cor geral letras, não especificada
-    document.body.style.color = "white";
-}
-
-// Chamar quando necessário
-aplicarTemaEscuro();
