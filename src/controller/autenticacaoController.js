e2206f89396fef017453b0fe47bfac9dade32084
MaeinesenieaM <david.costa.gomes71@gmail.com>
2025-06-25 13:57:29 -0300

    Renomeado req/res para request/response

--- src/controller/autenticacaoController.js
+++ src/controller/autenticacaoController.js
@@ -1,39 +1,39 @@
 const Usuario = require('../model/usuario'); // chamando tabela de usuarios
 
-async function autenticar(req, res){
+async function autenticar(request, response){
     const usuario = await Usuario.findOne({ //await garantir codigo sequencial (async)
         where: {
-        email: req.body.email, 
-        senha: req.body.senha
+        email: request.body.email,
+        senha: request.body.senha
     }
     });
     // grava a informação do usuario dentro da sessão 
     if(usuario !== null){
-        req.session.autorizado = true; //criando um valor temporario para mostrar que está logado na sessão
-        req.session.usuario = usuario;
-        res.redirect('/home'); // apos a validação retorna para a home page
+        request.session.autorizado = true; //criando um valor temporario para mostrar que está logado na sessão
+        request.session.usuario = usuario;
+        response.redirect('/home'); // apos a validação retorna para a home page
     }
     else{
         let erro_autenticacao = true;
-        res.render('index.html', {erro_autenticacao}); // caso ususario não logue, volta para a tela inicial com mensagem de erro
+        response.render('index.html', {erro_autenticacao}); // caso ususario não logue, volta para a tela inicial com mensagem de erro
     }
 }
 // verificar a veracidade da autentificação
-function verificarAutenticacao(req, res, next) {
-    if(req.session.autorizado){
+function verificarAutenticacao(request, response, next) {
+    if(request.session.autorizado){
         console.log("usuário autorizado");
         next();
     }
     else{
         console.log("usuário NÃO autorizado");
-        res.redirect('/'); // caso o usuario dê logout, irá ser encamidado para a pagina de origem 
+        response.redirect('/'); // caso o usuario dê logout, irá ser encamidado para a pagina de origem
     }   
 }
 
 // como as informações são gravadas na sessão, para sair e só destruir essa sessão
-function sair(req, res) {
-    req.session.destroy();
-    res.redirect('/');
+function sair(request, response) {
+    request.session.destroy();
+    response.redirect('/');
 }
 
 module.exports = {
--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -6,8 +6,8 @@ const usuarioController = require('../controller/usuarioController');
 router.post('/cadastrar_usuario', usuarioController.cadastrarUsuario);
 router.get('/api/usuarios', usuarioController.listarUsuarios);
 
-router.get('/cadastro_usuario', (req, res) => {
-    res.render('cadastrar_usuario.html');
+router.get('/cadastro_usuario', (request, response) => {
+    response.render('cadastrar_usuario.html');
 });
 
 
